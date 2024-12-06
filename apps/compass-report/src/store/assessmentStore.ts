6ce90b936215baaef39305c94554c8819c8790e1
Harry Dennen <h@harrydennen.com>
2024-12-06 13:04:18 -0500

    add exit confidence calc to assessment store

--- apps/compass-report/src/store/assessmentStore.ts
+++ apps/compass-report/src/store/assessmentStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 import { dataEntry } from '../types';
-import { ConfidenceLevel } from '../enums';
+import { ConfidenceLevel, ExitConfidenceKeys, ExitConfidenceLevel, QuestionAreaNames } from '../enums';
 
 interface AssessmentState {
   rawData: { [key: string]: string }[]; 
@@ -16,6 +16,7 @@ export interface AssessmentActions {
   setError: (error: string | null) => void;
   transformData: (transformer: (data: any[]) => dataEntry<dataEntry>[]) => void;
   getConfidenceData: () => {[key: string]: ConfidenceLevel[]};
+  getExitConfidenceByArea: <T>(area: QuestionAreaNames, asAverage: number) => {[key: string]: number[]} | {[key: string]: number} | T;
 }
 
 function calculateConfidence(assessmentData: dataEntry<dataEntry>[]): {[key: string]: ConfidenceLevel[]} {
@@ -55,6 +56,58 @@ export const useAssessmentStore = create<AssessmentState & AssessmentActions>((s
       return calculatedConfidenceData;
     }
     return confidenceData;
+  },
+  getExitConfidenceByArea: (area: QuestionAreaNames, asAverage: number) => {
+    const { transformedData } = get();
+    const exitConfidenceData: { [key: string]: number[] } = {};
+
+    const questionText = ExitConfidenceKeys[area as keyof typeof ExitConfidenceKeys];
+
+    transformedData.forEach(item => {
+      const exitConfidence = item[questionText];
+      if (!exitConfidence) {
+          console.warn('No exit confidence data found for ' + area);
+          return;
+      }
+  
+      Object.keys(exitConfidence).forEach(key => {
+          const confidenceLevel = exitConfidence[key];
+          if (Object.values(ExitConfidenceLevel).includes(confidenceLevel as ExitConfidenceLevel)) {
+          if (!exitConfidenceData[key]) {
+              exitConfidenceData[key] = [];
+          }
+  
+          exitConfidenceData[key].push(confidenceLevel as ExitConfidenceLevel);
+          }
+      });
+    });
+
+    if (asAverage === 1) {
+      let exitConfidenceDataAverageBySubSections: { [key: string]: number } = {};
+
+      Object.entries(exitConfidenceData).forEach(([key, values]) => {
+        const average = values.reduce((sum: number, val: number) => sum + ExitConfidenceLevel[val as keyof typeof ExitConfidenceLevel], 0) / values.length;
+        exitConfidenceDataAverageBySubSections[key] = average;
+      });
+
+      return exitConfidenceDataAverageBySubSections;
+    }
+
+    if (asAverage === 2) {
+      let exitConfidenceDataAverageByArea: number;
+      let totalValues = 0;
+      let totalEntries = 0;
+      Object.entries(exitConfidenceData).forEach(([key, values]) => {
+        totalValues += values.reduce((sum: number, val: number) => sum + ExitConfidenceLevel[val as keyof typeof ExitConfidenceLevel], 0);
+        totalEntries += values.length;
+      });
+
+      exitConfidenceDataAverageByArea = totalValues / totalEntries;
+
+      return exitConfidenceDataAverageByArea;
+    }
+
+    return exitConfidenceData;
   },
   setRawData: (data) => set({ rawData: data }),
   setLoading: (loading) => set({ isLoading: loading }),
