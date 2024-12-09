8b06a9fb259cb3bdd415692f6048853977019b1f
Harry Dennen <h@harrydennen.com>
2024-12-09 10:19:25 -0500

    add more colors

--- apps/compass-report/src/charts/colors.tsx
+++ apps/compass-report/src/charts/colors.tsx
@@ -1,7 +1,14 @@
+import { ExitConfidenceNames } from "../enums";
+
 export const colors = {
   actualKnowledgeBar: '#ffe5a9',
   actualKnowledgeAreaFill: 'rgba(255,205,86,.5)',
   actualKnowledgeAreaStroke: '#ffe5a9',
   exitConfidenceBar: '#82ca9d',
-  confidenceBar: '#8db1d3'
+  confidenceBar: '#8db1d3',
+  [ExitConfidenceNames.CompletelyUnsure]: '#ff7373',
+  [ExitConfidenceNames.SlightlyUnsure]: '#ffe5a9',
+  [ExitConfidenceNames.FairlySure]: '#82ca9d',
+  [ExitConfidenceNames.CompletelySure]: '#8db1d3',
+  legendText: '#000'
 }
